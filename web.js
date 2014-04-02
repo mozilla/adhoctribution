37db9aa927226e35ba249c257c4624ca028ee2d6
Adam Lofting <contactme@adamlofting.com>
2014-04-02 11:35:53 +0100

    boilerplate

--- web.js
+++ web.js
@@ -2,10 +2,21 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+// set up handlebars view engine
+var handlebars = require('express3-handlebars').create({
+  defaultLayout: 'main'
+});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// static
+app.use(express.static(__dirname + '/public'));
+
+// logging
 app.use(logfmt.requestLogger());
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('home');
 });
 
 var port = Number(process.env.PORT || 5000);
