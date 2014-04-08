506fd0525f050900af2cc700af7e37c607b36457
Adam Lofting <contactme@adamlofting.com>
2014-04-08 14:56:43 +0100

    Fixes #2 - add evidence field

--- sql/table.sql
+++ sql/table.sql
@@ -6,5 +6,6 @@ CREATE TABLE `contributions` (
   `mofo_team` varchar(45) NOT NULL,
   `data_bucket` varchar(45) NOT NULL,
   `description` varchar(255) NOT NULL,
+  `evidence` varchar(2000) DEFAULT NULL,
   UNIQUE KEY `no_dupes` (`contributor_id`,`contribution_date`,`mofo_team`,`data_bucket`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
